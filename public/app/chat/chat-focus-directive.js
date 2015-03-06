e71c1dc7faf153558d24a0f01edf2721170fc3f6
dsbalaban <dbalaban@P5RO-PC087.pentalog.com>
2015-03-06 13:24:15 +0200

    Fixed LESS and chat directive hover effect

--- public/app/chat/chat-focus-directive.js
+++ public/app/chat/chat-focus-directive.js
@@ -3,21 +3,22 @@
 
     define([], function() {
         var focusChat = function() {
+            var cssOptions = {
+                'transition': 'all 0.5s ease',
+                'bottom': '-30px'
+            };
             return {
                 restrict: 'A',
                 link: function ($scope, element, attrs) {
                     element.bind('focus', function () {
-                        element.parent().css({
-                            'transition': 'all 0.5s ease',
-                            'bottom': '0'
-                        });
+                        element.parent().css(cssOptions);
                     });
                     element.bind('blur', function() {
-                        element.parent().css({
-                            'transition': 'all 0.5s ease',
-                            'bottom': '-30px'
-                        });
+                        element.parent().css(cssOptions);
                     });
+                    element.bind('hover', function() {
+                        element.parent().css(cssOptions);
+                    })
                 }
             }
         };
